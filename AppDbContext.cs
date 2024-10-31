9e765b1c2842ab4c8172fd6249fca14b9ea87fbb
Muzola Ethberg <ethbergmuzola500@gmail.com>
2024-10-31 12:45:15 +0100

    Feat: Ajout de notre context pour la bdd

---
+++ AppDbContext.cs
@@ -0,0 +1,49 @@
+﻿using blogApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace blogApp
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Comment> Comments { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>()
+             .HasOne(p => p.User)
+             .WithMany(u => u.Posts)
+             .HasForeignKey(p => p.UserId)
+             .OnDelete(DeleteBehavior.Restrict); // Empêche la suppression d'un utilisateur s'il a des posts associés
+
+            modelBuilder.Entity<Comment>()
+               .HasOne(c => c.Post)
+               .WithMany(p => p.Comments)
+               .HasForeignKey(c => c.PostId)
+               .OnDelete(DeleteBehavior.Cascade); 
+
+            modelBuilder.Entity<Comment>()
+                .HasOne(c => c.User)
+                .WithMany(u => u.Comments)
+                .HasForeignKey(c => c.UserId)
+                .OnDelete(DeleteBehavior.Restrict); 
+
+            modelBuilder.Entity<Post>()
+               .HasOne(c => c.Category)
+               .WithMany(u => u.Posts)
+               .HasForeignKey(c => c.CategoryId)
+               .OnDelete(DeleteBehavior.Restrict); // Restrict pour éviter les cycles
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+
+
+    }
+}
