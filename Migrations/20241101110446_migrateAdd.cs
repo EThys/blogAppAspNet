0cfd1bbbc2a0e9640b67f80104d0b7a3492f2fe8
Muzola Ethberg <ethbergmuzola500@gmail.com>
2024-11-02 13:41:49 +0100

    Feat: Add new migration

---
+++ Migrations/20241101110446_migrateAdd.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace blogApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class migrateAdd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AccessToken",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExpiresIn",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccessToken",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ExpiresIn",
+                table: "Users");
+        }
+    }
+}
---
+++ Migrations/20241101140051_addSalt.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace blogApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class addSalt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserSalt",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserSalt",
+                table: "Users");
+        }
+    }
+}
